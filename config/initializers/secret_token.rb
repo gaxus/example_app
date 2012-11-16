4d60ca21407b253b71ed67f692bcfc9bfb206dba
L3 Info <l3info@ubuntu.(none)>
2012-11-16 02:14:42 -0800

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleApp::Application.config.secret_token = 'cc63d9da3a9fe01efcfc66db6f094645bcbd7284e14996477c0b2a891ff7a282631722a36cf963ab07cfca58b0a57be933d5e707570e72fcdfda94d9c33cccf1'
